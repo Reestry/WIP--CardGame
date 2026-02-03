f97989c03f0b6e05a25ac69ca3d54d6b501fd87d
Alexey <20it4.kurash.a@pdu.by>
2026-02-03 16:43:58 +0300

    Added enemy

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -1,22 +1,38 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyController : PlayableCard
 {
-    
+    [SerializeField] private List<PlayableCardObject> _card = new();
+
+    [SerializeField] private TMP_Text _damageText;
+    [SerializeField] private TMP_Text _healthText;
+
+    [SerializeField] private SpriteRenderer _faceSprite;
+
+    private void Start()
+    {
+        var rnd = Random.Range(0, _card.Count);
+
+        SetInfo(_card[rnd]);
+    }
+
+    private int _health;
+
     public void SetInfo(PlayableCardObject cardInfo)
     {
         _playableCardInfo = cardInfo;
 
-        _faceSprite = _playableCardInfo.FaceSprite;
+        _faceSprite.sprite = _playableCardInfo.FaceSprite;
         _backSprite = _playableCardInfo.BackSprite;
 
         _currentSuit = _playableCardInfo.CurrentSuit;
         _cost = _playableCardInfo.Cost;
-        _type = _playableCardInfo.Type;
-       // _spriteRenderer.sprite = _faceSprite;
+        _health = _cost * 2;
+
+        _healthText.text = _health.ToString();
+        _damageText.text = _cost.ToString();
     }
-    
-    
-    
-    
 }
